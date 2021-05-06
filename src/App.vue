330eb9289a12359ce7b6331c372782b12d197499
Kamile Pimenta <kamile.pimenta@outlook.com>
2021-05-06 11:23:25 -0300

    Estilização básica

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
-    <h1>{{ title }}</h1>
-    <ul class="list">
-      <li class="item" v-for="foto of fotos" :key="foto.id"></li>
-    </ul>
+    <div class="gallery">
+      <h1 class="title">{{ title }}</h1>
+      <ul class="list photos">
+        <li class="item" v-for="foto of fotos" :key="foto.id">
+          <div class="painel">
+            <h2 class="title">{{ foto.titulo }}</h2>
+            <div class="content">
+              <img class="img" :src="foto.url" :alt="foto.titulo">
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -17,6 +26,7 @@ export default {
     }
   },
   created() {
+    // Requisição das fotos
     this.$http.get('http://localhost:3000/v1/fotos')
     .then(data => data.json() )
     .then(
@@ -27,31 +37,55 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;0,900;1,400&display=swap');
 
-h1, h2 {
-  font-weight: normal;
-}
+  .gallery {
+    max-width: 1160px;
+    margin: 0 auto;
+    padding: 60px 0;
+    font: 16px 'Montserrat', sans-serif;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .title {
+    color: #673ab7;
+    font-size: 32px;
+    font-weight: 900;
+    text-align: center;
+    text-transform: uppercase;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .photos {
+    display: flex;
+    align-items: stretch;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    list-style: none;
+  }
 
-a {
-  color: #42b983;
-}
+  .photos .item {
+    width: calc(33.33% - 20px);
+    margin: 10px;
+  }
+
+  .photos .painel {
+    border-radius: 5px;
+    overflow: hidden;
+  }
+
+  .photos .title {
+    margin: 0;
+    padding: 15px 20px;
+    background-color: #ede7f6;
+    color: #444;
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  .photos .img {
+    display: block;
+    width: 100%;
+    max-width: 100%;
+    border: 0;
+  }
 </style>
