79b96a150e8ab16f466f50c5d2c9102fcbe2c8c5
Kamile Pimenta <kamile.pimenta@outlook.com>
2021-05-06 10:48:30 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <ul class="list">
+      <li class="item" v-for="foto of fotos" :key="foto.id"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'AluraPic',
+      fotos: [],
+    }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+    .then(data => data.json() )
+    .then(
+      imgs => this.fotos = imgs, 
+      err => console.error(err)
+    );
+  },
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
